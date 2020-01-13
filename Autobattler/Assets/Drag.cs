6b68466727b6884b7f967f35d03249058f13d310
jostarly <ostarly.james@gmail.com>
2020-01-13 16:52:50 -0600

    Update Drag.cs
    fixed with tyler's good boi script

--- Autobattler/Assets/Drag.cs
+++ Autobattler/Assets/Drag.cs
@@ -9,63 +9,16 @@ using UnityEngine;
 public class Drag : MonoBehaviour
 
 {
-
-    private Vector3 mOffset;
-
-
-
-    private float mZCoord;
-
-
-
-    void OnMouseDown()
-
-    {
-
-        mZCoord = Camera.main.WorldToScreenPoint(
-
-            gameObject.transform.position).z;
-
-
-
-        // Store offset = gameobject world pos - mouse world pos
-
-        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-
-    }
-
-
-
-    private Vector3 GetMouseAsWorldPoint()
-
-    {
-
-        // Pixel coordinates of mouse (x,y)
-
-        Vector3 mousePoint = Input.mousePosition;
-
-
-
-        // z coordinate of game object on screen
-
-        mousePoint.z = mZCoord;
-
-
-
-        // Convert it to world points
-
-        return Camera.main.ScreenToWorldPoint(mousePoint);
-
-    }
-
-
-
     void OnMouseDrag()
 
     {
-
-        transform.position = GetMouseAsWorldPoint() + mOffset;
-
+        Plane plane = new Plane(Vector3.up, new Vector3(0, 1, 0));
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
     }
 
 }
